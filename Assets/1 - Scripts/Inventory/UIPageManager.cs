330495aa213839fac12e3bcf70ac732e55028d5e
Valentin <111918150+CuteShirou@users.noreply.github.com>
2025-07-16 15:46:53 +0200

    [ADD]
    - Money icone
    - UIPageManager

---
+++ Assets/1 - Scripts/Inventory/UIPageManager.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class UIPageManager : MonoBehaviour
+{
+    public GameObject PageInventory;
+    public GameObject PageCrafting;
+    public GameObject PageStats;
+    public GameObject PageSkillTree;
+    
+    void Start()
+    {
+        SetActivePage(PageInventory); // Default page
+    }
+    
+    public void ShowInventory()
+    {
+        HideAllPages();
+        SetActivePage(PageInventory);
+    }
+    
+    public void ShowCrafting()
+    {
+        HideAllPages();
+        SetActivePage(PageCrafting);
+    }
+    
+    public void ShowStats()
+    {
+        HideAllPages();
+        SetActivePage(PageStats);
+    }
+    
+    public void ShowSkillTree()
+    {
+        HideAllPages();
+        SetActivePage(PageSkillTree);
+    }
+    
+    private void HideAllPages()
+    {
+        PageInventory.SetActive(false);
+        PageCrafting.SetActive(false);
+        PageStats.SetActive(false);
+        PageSkillTree.SetActive(false);
+    }
+    
+    private void SetActivePage(GameObject ActivePage)
+    {
+        PageInventory.SetActive(ActivePage == PageInventory);
+        PageCrafting.SetActive(ActivePage == PageCrafting);
+        PageStats.SetActive(ActivePage == PageStats);
+        PageSkillTree.SetActive(ActivePage == PageSkillTree);
+    }
+}
