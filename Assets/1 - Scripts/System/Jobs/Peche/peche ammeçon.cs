0b9c63d11aed51fa3207b4a6f22889682e8e2316
etoyashii <147041505+etoyashii@users.noreply.github.com>
2025-07-18 11:11:32 +0200

    [END] peche

--- Assets/1 - Scripts/System/Jobs/Peche/peche ammeçon.cs
+++ Assets/1 - Scripts/System/Jobs/Peche/peche ammeçon.cs
@@ -6,19 +6,18 @@ using UnityEngine;
 public class peche : MonoBehaviour
 {
     // Start is called before the first frame update
-    
+    public GameObject progressBar; // Reference to the progress bar GameObject
     public float x ;
     public float y;
     public float speed;
+    public RectTransform poissonRect; // Assigne le RectTransform du poisson dans l’inspecteur
+    public pecheprogress progressBarScript;
     void Start()
     {
         RectTransform rt = gameObject.GetComponent<RectTransform>();
         x = rt.anchoredPosition.x;
         y = rt.anchoredPosition.y;
-       speed = 3f; // Speed of movement
-        
-
-        // Set the initial position of the game object
+        speed = 3f; 
         rt.anchoredPosition = new Vector2(x, y);
     }
 
@@ -26,6 +25,9 @@ public class peche : MonoBehaviour
     void Update()
     {
         RectTransform rt = gameObject.GetComponent<RectTransform>();
+        
+        
+        
         if (Input.GetMouseButton(0) && y <= 229)
         {
             y -= speed;
@@ -37,5 +39,25 @@ public class peche : MonoBehaviour
             if (y > 229) y = 229;
         }
         rt.anchoredPosition = new Vector2(x, y);
+        
+        if (RectsOverlap(rt, poissonRect))
+        {
+            progressBarScript.AugmenterHauteur(1f);
+        }
+    }
+
+    
+
+    bool RectsOverlap(RectTransform a, RectTransform b)
+    {
+        Vector3[] aCorners = new Vector3[4];
+        Vector3[] bCorners = new Vector3[4];
+        a.GetWorldCorners(aCorners);
+        b.GetWorldCorners(bCorners);
+
+        Rect rectA = new Rect(aCorners[0], aCorners[2] - aCorners[0]);
+        Rect rectB = new Rect(bCorners[0], bCorners[2] - bCorners[0]);
+
+        return rectA.Overlaps(rectB);
     }
 }
