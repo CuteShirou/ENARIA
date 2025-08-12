42fa2634e53246c90c4b6068cb0694dac5ca11b9
Valentin <111918150+CuteShirou@users.noreply.github.com>
2025-08-12 15:40:00 +0200

    Fix Merge

--- Assets/0 - Scripts/System/scene changer.cs
+++ Assets/0 - Scripts/System/scene changer.cs
@@ -1,13 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class scenechanger : MonoBehaviour
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class SceneChanger : MonoBehaviour
 {
-    
-    public string sceneName ;
-    public void change_scene()
+#if UNITY_EDITOR
+    [SerializeField, Tooltip("Glisser-déposer l’asset de scène ici. (Éditeur uniquement)")]
+    private SceneAsset sceneAsset;
+#endif
+
+    private string sceneName;
+
+#if UNITY_EDITOR
+    private void OnValidate()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+        if (sceneAsset != null)
+        {
+            sceneName = sceneAsset.name;
+        }
+    }
+#endif
+
+    public void ChangeScene()
+    {
+        if (string.IsNullOrEmpty(sceneName))
+        {
+            Debug.LogError("[SceneChanger] Aucun asset de scène assigné dans l’inspecteur.");
+            return;
+        }
+        SceneManager.LoadScene(sceneName);
     }
 }
