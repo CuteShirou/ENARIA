432acfc7264cad1ee0749b5ee0475572dba289fc
etoyashii <147041505+etoyashii@users.noreply.github.com>
2025-08-26 17:19:32 +0200

    [ADD] 5pnj+5dialogues

--- Assets/0 - Scripts/System/OnClick3D.cs
+++ Assets/0 - Scripts/System/OnClick3D.cs
@@ -12,16 +12,37 @@ public class OnClick3D : MonoBehaviour
     [SerializeField] private string arbreTag = "arbre";
     [SerializeField] private string rocherTag = "rocher";
     [SerializeField] private string planteTag = "plante";
+    [SerializeField] private string pnjspawnTag = "pnjSpawn";
+    [SerializeField] private string pnjforge1Tag = "pnjforge1";
+    [SerializeField] private string pnjforge2Tag = "pnjforge2";
+    [SerializeField] private string pnjshopTag = "pnjshop";
+    [SerializeField] private string pnjtaverneTag = "pnjtaverne";
     public GameObject cereales;
     public GameObject bois;
     public GameObject minerai;
     public GameObject poisson;
     public GameObject plantes;
+    public GameObject dialoguespawn;
+    public GameObject dialogueforge1;
+    public GameObject dialogueforge2;
+    public GameObject dialogueshop;
+    public GameObject dialoguetaverne;
     public static bool cerealesIsActive = false;
     public static bool boisIsActive = false;
     public static bool mineraiIsActive = false;
     public static bool poissonIsActive = false;
     public static bool plantesIsActive = false;
+    public static bool dialoguespawnIsActive = false;
+    public static bool dialogueforge1IsActive = false;
+    public static bool dialogueforge2IsActive = false;
+    public static bool dialogueshopIsActive = false;
+    public static bool dialoguetaverneIsActive = false;
+    public Item bletoguive;
+    public Item saumontoguive;
+    public Item tulipetoguive;
+    public Item ortoguive;
+    public Item boistoguive;
+    
     void Start()
     {
         
@@ -38,6 +59,7 @@ public class OnClick3D : MonoBehaviour
             cerealesIsActive = false;
             Debug.Log("céréales récoltées");
             cerealesprogress.height = 0; 
+            InventoryUtil.AddItemToFirstEmpty(bletoguive);
         }
         if (minijeuplantes.height >= 599)
         {
@@ -46,6 +68,7 @@ public class OnClick3D : MonoBehaviour
             Debug.Log("plantes récoltées");
             minijeuplantes.height = 0;
             TPC.enabled = true;
+            InventoryUtil.AddItemToFirstEmpty(tulipetoguive);
         }
         if (boisprogress.height >= 599)
         {
@@ -55,6 +78,7 @@ public class OnClick3D : MonoBehaviour
             boisgameplay.isstarting = true; 
             boisprogress.height = 0;
             TPC.enabled = true;
+            InventoryUtil.AddItemToFirstEmpty(boistoguive);
         }
 
         if (pecheprogress.height >= 599)
@@ -64,6 +88,7 @@ public class OnClick3D : MonoBehaviour
             Debug.Log("poisson récolté");
             pecheprogress.height = 0;
             TPC.enabled = true;
+            InventoryUtil.AddItemToFirstEmpty(saumontoguive);
         }
 
         if (minagebutton.Win == true)
@@ -73,7 +98,26 @@ public class OnClick3D : MonoBehaviour
             Debug.Log("minerai récolté");
             minagebutton.Win = false;
             TPC.enabled = true;
+            InventoryUtil.AddItemToFirstEmpty(ortoguive);
         }
+        
+        if (ButtonEndDialogue.isclicked == true)
+        {
+            dialogueforge1.SetActive(false);
+            dialogueforge2.SetActive(false);
+            dialoguespawn.SetActive(false);
+            dialogueshop.SetActive(false);
+            dialoguetaverne.SetActive(false);
+            dialoguespawnIsActive = false;
+            dialogueforge1IsActive = false;
+            dialogueforge2IsActive = false;
+            dialogueshopIsActive = false;
+            dialoguetaverneIsActive = false;
+            Debug.Log("dialogue terminé");
+            ButtonEndDialogue.isclicked = false;
+            TPC.enabled = true;
+        }
+        
       
     }
     private void OnTriggerEnter(Collider other)
@@ -146,13 +190,80 @@ public class OnClick3D : MonoBehaviour
             
             if (TPC != null)
             {
-                
                 Debug.Log("canne tcp");
                 TPC.IsInCombat = false;
                 TPC.ForceStopMovement();
                 TPC.enabled = false;
             }
         }
+        if (other.CompareTag(pnjspawnTag))
+        {
+            dialoguespawn.SetActive(true);
+            Debug.Log("collide avec pnj spawn");
+            dialoguespawnIsActive = true;
+            
+            if (TPC != null)
+            {
+                Debug.Log("pnj spawn tcp");
+                TPC.IsInCombat = false;
+                TPC.ForceStopMovement();
+                TPC.enabled = false;
+            }
+        }
+
+        if (other.CompareTag(pnjforge1Tag))
+        {
+            dialogueforge1.SetActive(true);
+            Debug.Log("collide avec pnj forge1");
+            dialogueforge1IsActive = true;
+            if (TPC != null)
+            {
+                Debug.Log("pnj forge1 tcp");
+                TPC.IsInCombat = false;
+                TPC.ForceStopMovement();
+                TPC.enabled = false;
+            }
+        }
+        if (other.CompareTag(pnjforge2Tag))
+        {
+            dialogueforge2.SetActive(true);
+            Debug.Log("collide avec pnj forge2");
+            dialogueforge2IsActive = true;
+            if (TPC != null)
+            {
+                Debug.Log("pnj forge2 tcp");
+                TPC.IsInCombat = false;
+                TPC.ForceStopMovement();
+                TPC.enabled = false;
+            }
+        }
+        if (other.CompareTag(pnjshopTag))
+        {
+            dialogueshop.SetActive(true);
+            Debug.Log("collide avec pnj shop");
+            dialogueshopIsActive = true;
+            if (TPC != null)
+            {
+                Debug.Log("pnj shop tcp");
+                TPC.IsInCombat = false;
+                TPC.ForceStopMovement();
+                TPC.enabled = false;
+            }
+        }
+        if (other.CompareTag(pnjtaverneTag))
+        {
+            dialoguetaverne.SetActive(true);
+            Debug.Log("collide avec pnj taverne");
+            dialoguetaverneIsActive = true;
+            if (TPC != null)
+            {
+                Debug.Log("pnj taverne tcp");
+                TPC.IsInCombat = false;
+                TPC.ForceStopMovement();
+                TPC.enabled = false;
+            }
+        }
+
     }
  
 }
