3f35f94fcb8e3fce4cbbadf72a696ad12fb20cb4
etoyashii <147041505+etoyashii@users.noreply.github.com>
2025-08-28 14:45:57 +0200

    [ADD] Start dialogue manager

---
+++ Assets/0 - Scripts/System/dialogues/Dialogue.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    public DialoguesString dialoguestring;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
